b4047e0bb6ab879a83be222ad3d85c3620bdf420
Devraj Rajput <rajkirar2002.pc@gmail.com>
2024-08-13 17:45:03 +0530

    Protfolio Setup

---
+++ src/app/_components/Header.tsx
@@ -0,0 +1,37 @@
+import React from "react";
+import {
+  Sheet,
+  SheetContent,
+  SheetDescription,
+  SheetHeader,
+  SheetTitle,
+  SheetTrigger,
+} from "@/components/ui/sheet";
+
+const Header = () => {
+  return (
+    <header className=" py-4">
+      <div className=" container flex justify-between items-center">
+        <div>
+          <span className=" text-3xl font-semibold">Devraj Rajput</span>
+        </div>
+        <div>
+          <Sheet>
+            <SheetTrigger>Open</SheetTrigger>
+            <SheetContent>
+              <SheetHeader>
+                <SheetTitle>Are you absolutely sure?</SheetTitle>
+                <SheetDescription>
+                  This action cannot be undone. This will permanently delete
+                  your account and remove your data from our servers.
+                </SheetDescription>
+              </SheetHeader>
+            </SheetContent>
+          </Sheet>
+        </div>
+      </div>
+    </header>
+  );
+};
+
+export default Header;
